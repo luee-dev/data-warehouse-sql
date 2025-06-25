abc23559f4395a7586d9d2a85357328ad0ca7994
luee-dev <151563449+luee-dev@users.noreply.github.com>
2025-06-25 10:25:35 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -16,7 +16,7 @@ WARNING:
 USE master;
 GO
 
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = "DataWarehouse")
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWarehouse;
